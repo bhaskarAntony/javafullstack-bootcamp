deb5d338d4e27468cc696ff2065f7f0ca0035fda
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-02-07 15:41:49 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -6,9 +6,28 @@ import Header from './components/Header';
 import { ToastContainer } from 'react-toastify';
 import FreeRegistration from './components/FreeRegistration';
 import Scroll from './components/Scroll';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-JVNG3G2GY2');
+
 
 function App() {
+
+  useEffect(() => {
+    // Extract UTM parameters from the URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const utmSource = urlParams.get('utm_source');
+    const utmMedium = urlParams.get('utm_medium');
+    const utmCampaign = urlParams.get('utm_campaign');
+
+    // Track pageview with UTM parameters
+    ReactGA.pageview(window.location.pathname + window.location.search, [], {
+      utm_source: utmSource,
+      utm_medium: utmMedium,
+      utm_campaign: utmCampaign,
+    });
+  }, []);
   return (
     <div className="App">
       {/* <Scroll/> */}
